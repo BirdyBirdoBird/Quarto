6139028cc0ba753d0d3d5170affe6b7427aaa3ff
Team-Sycamore <teamguy5614@gmail.com>
2025-02-09 10:51:40 +0200

    FIrst Commit

---
+++ src/main/java/Quarto/Project.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Quarto;
+
+/**
+ *
+ * @author david
+ */
+public class Project {
+
+    public static void main(String[] args) {
+    }
+}
